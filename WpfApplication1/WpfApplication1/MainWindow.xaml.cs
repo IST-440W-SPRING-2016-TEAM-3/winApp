b4c8d5214be7e84610c499610608e27c5ad38be8
bvx5009 <baxadirxudayberdiev@gmail.com>
2016-02-23 21:41:43 -0500

    WinApp added

---
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (patientInfo != null && patientInfo.IsSelected)
+            {
+                headLabel.Content = "Patient Info";
+            }
+            if (currentIssues != null && currentIssues.IsSelected)
+            {
+                headLabel.Content = "Current Health Issues";
+            }
+            if (medications != null && medications.IsSelected)
+            {
+                headLabel.Content = "Medications";
+            }
+            if (allergies != null && allergies.IsSelected)
+            {
+                headLabel.Content = "Allergies";
+            }
+            if (immunizations != null && immunizations.IsSelected)
+            {
+                headLabel.Content = "Immunizations";
+            }
+            if (historySummary != null && historySummary.IsSelected)
+            {
+                headLabel.Content = "History Summary";
+            }
+            if (testResults != null && testResults.IsSelected)
+            {
+                headLabel.Content = "Test Results";
+            }
+        }
+
+
+
+    }
+}
